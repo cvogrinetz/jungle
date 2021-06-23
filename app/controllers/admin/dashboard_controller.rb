286b148aadefd73835ec1b9075b7c4936bc7f82f
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-06-23 06:20:48 +0000

    Add instance variable to dashboard_controller AND render count for product and category to Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD'] 
+  
+    def show
+      @products = Product.order(id: :desc).all
+      @category = Category.order(id: :desc).all
+      # puts "PRODUCT COUNT----------#{@products.count}"
+      # puts "CATEGORY COUNT----------#{@category.count}"
+    end
+    
 
-
-  def show
-  end
 end
