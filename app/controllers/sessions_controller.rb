f2b70952593d24452f1e33fe9c874dbdd04e5d11
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-06-24 00:17:55 +0000

    Add basic styles to login and register forms as well as nav-bar styles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 end
